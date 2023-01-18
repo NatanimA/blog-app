03437f554dd2f93fec5e1322c74dfe5f716fd099
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-18 16:46:23 +0300

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,12 @@ class User < ApplicationRecord
   # it will try to reterive the informations by given author ID.
   scope :find_posts_amount, ->(id) { select(:posts_counter).where(id:) }
 
-
   # This method will take the authors Id as parameter, and updated count value
   # It will increment the posts_counter by the given count value the.
   def self.update_posts_counter(id, posts_counter)
     count = posts_counter
     User.where(id:).update(posts_counter: count)
   end
-
 
   # Here we can get posts that belongs to each user by just passing the ID of the author
   scope :recent_posts, ->(id) { Post.posts_by_author_id(id) }
