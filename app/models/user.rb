845d7d966fd4f8d4df3f3a89f1e641cfe1784eab
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-19 15:22:37 +0300

    User Models validation to check fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name , presence: true
+  validates :name , presence: true, length: {minimum:1, maximum:20}
   validates :posts_counter , comparison: { greater_than_or_equal_to: 0}, numericality: {only_integer:true}
 
 
