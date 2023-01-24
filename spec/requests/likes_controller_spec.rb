fb9136a1823f32252994c18bf2d562edaf82f8bd
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-24 19:20:07 +0300

    likes controller specs insertion

---
+++ spec/requests/likes_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+Rails.describe LikesController, :type => :request do
+  context "Action new like" do
+    it "Shoud make successful request" do
+      get new_like_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "Should render the correct template" do
+      get new_like_path
+      expect(response).to render_template(:new)
+    end
+
+    it "Shoud return the correct response body" do
+      get new_like_path
+      expect(response.body).to include("<!DOCTYPE html>")
+    end
+  end
+
+  context "Action on creating a new like" do
+    before :all do
+      @like = Like.new(author_id: 1, post_id: 1)
+    end
+    it "Should make successful request" do
+      post likes_path(@like)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should render the correct template" do
+      post likes_path(@like)
+      expect(response).to render_template(:new)
+    end
+
+    it "Should contain the correct response body" do
+      post likes_path(@like)
+      expect(response.body).to include("<!DOCTYPE html>")
+    end
+  end
+end
