d20522a221e480b283d3b1a3d5cd579886580c1d
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-25 16:21:06 +0300

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.all().where(author:params[:user_id])
+    @post = Post.all.where(author: params[:user_id])
   end
 
   def show
-    @post = Post.find_by(author:params[:user_id],id:params[:id])
+    @post = Post.find_by(author: params[:user_id], id: params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all()
+    @user = User.all
   end
 
   def show
