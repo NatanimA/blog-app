29d94c484f6e764cc9ceea4ea1994a32478feac6
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-26 18:49:28 +0300

    Coments Controller Create method insertion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,44 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(params[:comment])
-    if @comment.save
-      flash[:success] = 'comment successfully created'
-      redirect_to @comment
-    else
-      flash[:error] = 'Something went wrong'
-      render 'new'
+    @comment = Comment.new(params.require(:new_comment).permit(:text))
+    author = current_user()
+    post = Post.find(params[:post_id])
+    @comment.author =author
+    @comment.post = post
+
+    #Increase Comments Counter
+    comments_counter = Post.find_comments_amount(post.id).map(&:comments_counter)
+    comments_counter = comments_counter[0]
+    comments_counter = if comments_counter == nil
+                          1
+                       else
+                        comments_counter + 1
+                       end
+    Post.update_comments_counter(post.id,comments_counter)
+
+    respond_to do |format|
+      format.html do
+          if @comment.save
+            flash[:success] = 'comment successfully created'
+            redirect_to user_post_path(author.id,post.id)
+          else
+            flash[:error] = 'Something went wrong one or more field is empty'
+            comment = Comment.new
+            @user = current_user
+            respond_to do |format|
+              format.html { redirect_to request.referrer ,locals: {comment: comment} }
+            end
+          end
+      end
     end
+
   end
 
   def new
-    render 'new'
+    comment = Comment.new
+    @user = current_user
+    respond_to do |format|
+      format.html { render 'new', locals: { comment: comment} }
+    end
   end
 end
