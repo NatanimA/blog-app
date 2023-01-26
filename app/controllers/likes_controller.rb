1e9d67535b2ab2649cc8a2a654e7c224e233c791
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-26 18:50:01 +0300

    Likes controller create method insertion

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,34 @@
 class LikesController < ApplicationController
-  def new
-    render 'new'
-  end
-
   def create
-    @like = Like.new(params[:like])
-    if @like.save
-      flash[:success] = 'Like successfully created'
-      redirect_to @like
-    else
-      flash[:error] = 'Something went wrong'
-      render 'new'
+    @like = Like.new
+    @like.author = current_user()
+    post = Post.find(params[:post_id])
+    @like.post =post
+
+    #Update the likes counter
+    likes_counter = Post.find_likes_amount(post.id).map(&:likes_counter)
+    likes_counter = likes_counter[0]
+
+    likes_counter = if likes_counter == nil
+                      1
+                    else
+                      likes_counter + 1
+                    end
+    Post.update_likes_counter(post.id,likes_counter)
+
+    respond_to do |format|
+      format.html do
+        if @like.save
+          flash[:success] = 'Like successfully created'
+          redirect_to user_path(post.author.id)
+        else
+          flash[:error] = 'Something went wrong could not update likes'
+          respond_to do |format|
+            format.html { redirect_to request.referrer}
+          end
+        end
+      end
     end
+
   end
 end
