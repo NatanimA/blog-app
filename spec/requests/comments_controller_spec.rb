6f18caf2feb4b3a6cd097de66bf3cff4c549c621
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-30 13:01:30 +0300

    Fix unit testings

--- spec/requests/comments_controller_spec.rb
+++ spec/requests/comments_controller_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 Rails.describe CommentsController, type: :request do
   context 'Action on displaying form for comments' do
     it 'Should make successful request' do
-      get new_comment_path
+      get new_post_comment_path(1)
       expect(response).to have_http_status(:ok)
     end
 
     it 'Should render the correct template' do
-      get new_comment_path
+      get new_post_comment_path(1)
       expect(response).to render_template(:new)
     end
 
     it 'Should match the response body' do
-      get new_comment_path
+      get new_post_comment_path(1)
       expect(response.body).to include('<!DOCTYPE html>')
     end
   end
@@ -23,13 +23,13 @@ Rails.describe CommentsController, type: :request do
       @comment = Comment.new(text: 'Hello there!', post_id: 1, author_id: 1)
     end
     it 'Should make a successful request' do
-      post comments_path(@comment)
-      expect(response).to have_http_status(:ok)
+      post post_comments_path(:post_id => 2), :params => {:new_comment => {:text => 'Hello there!', :post_id => 1, :author_id => 1}}
+      expect(response).to have_http_status(:found)
     end
 
     it 'Should match the reponse body' do
-      post comments_path(@comment)
-      expect(response.body).to include('<!DOCTYPE html>')
+      post post_comments_path(:post_id => 1), :params => {:new_comment => {:text => 'Hello there!', :post_id => 1, :author_id => 1}}
+       expect(response.content_type).to eq "text/html; charset=utf-8"
     end
   end
 end
--- spec/requests/likes_controller_spec.rb
+++ spec/requests/likes_controller_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 Rails.describe LikesController, type: :request do
   context 'Action new like' do
     it 'Shoud make successful request' do
-      get new_like_path
+      get new_post_like_path(:post_id => 1)
       expect(response).to have_http_status(:ok)
     end
 
     it 'Should render the correct template' do
-      get new_like_path
+      get new_post_like_path(:post_id => 1)
       expect(response).to render_template(:new)
     end
 
     it 'Shoud return the correct response body' do
-      get new_like_path
+      get new_post_like_path(:post_id => 1)
       expect(response.body).to include('<!DOCTYPE html>')
     end
   end
@@ -23,18 +23,13 @@ Rails.describe LikesController, type: :request do
       @like = Like.new(author_id: 1, post_id: 1)
     end
     it 'Should make successful request' do
-      post likes_path(@like)
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'should render the correct template' do
-      post likes_path(@like)
-      expect(response).to render_template(:new)
+      post post_likes_path(:post_id => 1), :params => { :author_id => 1, :post_id => 1}
+      expect(response).to have_http_status(:found)
     end
 
     it 'Should contain the correct response body' do
-      post likes_path(@like)
-      expect(response.body).to include('<!DOCTYPE html>')
+      post post_likes_path(:post_id => 1), :params => { :author_id => 1, :post_id => 1}
+      expect(response.content_type).to eq "text/html; charset=utf-8"
     end
   end
 end
