5e733ce3f61e50c4729af789e8452c84d053393f
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-31 18:59:23 +0300

    Intialize Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   def current_user
     @user = User.first
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
+  root "user#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
 
-  resources :users do
-    resources :posts ,only: [:index,:show,:edit,:new,:create]
-  end
+#   get "/users/sign_up", to: "users#new"
 
-  resources :posts do
-    resources :comments, :likes , only: [:new,:create]
-  end
+devise_scope :user do
+  root to: 'devise/registrations#new'
 end
+
+devise_scope :user do
+  delete 'logout', to: 'devise/sessions#destroy'
+end
+
+   resources :users do
+     resources :posts ,only: [:index,:show,:edit,:new,:create]
+   end
+
+   resources :posts do
+     resources :comments, :likes , only: [:new,:create]
+   end
+ end
