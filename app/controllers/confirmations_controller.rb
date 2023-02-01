b48e1b6c7c560c9469cf0b93df9d550bc09358c0
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-01 11:56:34 +0300

    Insert Authentication ,confirm & regis controllers

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,20 @@
+class ConfirmationsController < Devise::ConfirmationsController
+  private
+  # def after_confirmation_path_for(resource_name, resource)
+  #   sign_in(resource) # In case you want to sign in the user
+  #   your_new_after_confirmation_path
+  # end
+
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+    #render "show"
+  end
+end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,7 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def after_inactive_sign_up_path_for(resource)
+    # cofirm_email_sent_path
+     new_user_confirmation_path
+  end
+end
