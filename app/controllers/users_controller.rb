6e24eeff41a6cde2b7f723eb34c21fc07cf7c6a2
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-01 19:46:29 +0300

    add post btn conditions insertion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @btn = if current_user == @user
+              true
+           else
+              false
+           end
     @posts = Post.includes(:author).where(author: { id: params[:id] }).order(created_at: 'DESC').first(3)
   end
+
+  def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+end
 end
